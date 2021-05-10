6a8aabaa0e845744ff73dcfb0700cc88036b3c1e
avinash0705 <avinash7may@gmail.com>
2021-05-10 12:01:31 +0530

    basic template created

---
+++ src/app/pages/analytics-page/analytics-page-routing.module.ts
@@ -0,0 +1,18 @@
+import { AnalyticsPageComponent } from './analytics-page.component';
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: AnalyticsPageComponent,
+        data: { shouldReuse: true, key: 'analytics' },
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class AnalyticsPageRoutingModule {}
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+
+@Component({
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.scss'],
+})
+export class LoginPageComponent implements OnInit {
+    name: string;
+    email: string;
+    password: string;
+    errorMessage: string;
+    loginUI = true;
+    signUpUI = false;
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    ngOnInit() {
+        this.errorMessage = '';
+        if (this.authService.isLogged()) {
+            this.navigateTo();
+        }
+    }
+
+    public async login(email: string, password: string) {
+        try {
+            const url = (await this.authService.mockLogin(
+                email,
+                password,
+            )) as string;
+            this.navigateTo(url);
+        } catch (e) {
+            this.errorMessage = 'Wrong Credentials!';
+            console.error('Unable to Login!\n', e);
+        }
+    }
+
+    public async signup(name: string, email: string, password: string) {
+        try {
+            const url = (await this.authService.signup(
+                name,
+                email,
+                password,
+            )) as string;
+            console.log('reutnr from auth signup', url);
+            this.changeToLoginUI();
+        } catch (e) {
+            this.errorMessage = 'Something Went Wrong!';
+            console.error('Unable to Signup!\n', e);
+        }
+    }
+
+    public navigateTo(url?: string) {
+        url = url || 'nav';
+        this.router.navigate([url], { replaceUrl: true });
+    }
+
+    changeToSignupUi() {
+        this.loginUI = false;
+        this.signUpUI = true;
+    }
+
+    changeToLoginUI() {
+        this.signUpUI = false;
+        this.loginUI = true;
+    }
+}
